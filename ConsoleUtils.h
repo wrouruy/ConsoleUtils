016caf4534a6fb7b2f71160b013a0876679c2642
wrouruy <wroyruy@gmail.com>
2025-07-29 20:48:47 +0300

    Add consoleGetCh, consoleGetStr

--- ConsoleUtils.h
+++ ConsoleUtils.h
@@ -1,11 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #ifdef _WIN32
     #include <windows.h>
 #else
     #include <sys/ioctl.h>
+    #include <unistd.h>
+    #include <termios.h>
     #include <unistd.h>
 #endif
 
@@ -52,4 +55,65 @@ struct ConsoleSize consoleGetSize(void)
         size.column = w.ws_col;
     #endif
     return size;
+}
+
+char consoleGetCh(void)
+{
+    #ifdef _WIN32
+        HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
+        DWORD mode = 0;
+
+        // Disable buffering
+        GetConsoleMode(hInput, &mode);
+        SetConsoleMode(hInput, mode & ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT));
+
+        INPUT_RECORD input;
+        DWORD read;
+
+        unsigned char result;        
+        while(1) {
+            ReadConsoleInput(hInput, &input, 1, &read);
+            if (input.EventType == KEY_EVENT && input.Event.KeyEvent.bKeyDown) {
+                result = input.Event.KeyEvent.uChar.AsciiChar;
+                break;
+            }
+        }
+
+        SetConsoleMode(hInput, mode);
+        return result;
+    #else
+        struct termios old_tio, new_tio;
+        unsigned char result;
+
+        // save current settings of terminal
+        tcgetattr(STDIN_FILENO, &old_tio);
+
+        // set new settings
+        new_tio = old_tio;
+        new_tio.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+
+        // read symbol
+        result = getchar();
+
+        // fix settings
+        tcsetattr(STDIN_FILENO, TCSANOW, &old_tio);
+
+        return result;
+    #endif
+}
+
+void consoleGetStr(char *address, unsigned int length)
+{
+    unsigned int i = 0;
+    char ch;
+
+    while (i < length - 1) {
+        ch = consoleGetCh();
+        if (ch == '\n') break;
+
+        address[i++] = ch;
+        printf("%c", ch);
+    }
+    address[i] = '\0';
 }
